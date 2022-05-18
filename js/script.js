b5d7c13d179a28f79582e625540dc9b4684107e7
Carlos Tabangay <carlos_tabangay@yahoo.com>
2022-05-19 03:01:07 +0800

    Temporary done and delete function

--- js/script.js
+++ js/script.js
@@ -10,33 +10,24 @@ function addItem(item) {
 		time: currentTime,
 	};
 
-	todoItems.push(todo);
-	// console.log(todoItems);
+	todoItems.push(todo); // console.log(todoItems);
 }
 
 function toggleDone(key) {
 	const index = todoItems.findIndex((item) => item.id === Number(key));
-	todoItems[index].checked = !todoItems[index.checked];
-	// renderTodo(todoItems[index]);
-	return index;
+	todoItems[index].checked = !todoItems[index].checked;
+	renderTodo(todoItems[index]);
 }
 
-// ========= TESTS SECTION ==========
+function deleteTodo(key) {
+	const index = todoItems.findIndex((item) => item.id === Number(key));
+	todoItems[index].checked = !todoItems[index].checked;
+	renderTodo(todoItems[index]);
+}
+
+// ========= TESTS ==========
 
 addItem("todo item #1");
-// addItem("todo item #2");
-// addItem("todo item #3");
-// addItem("todo item #4");
-
-// array.map(function(currentValue, index, arr), thisValue)
-// console.log(
-// 	todoItems.map((todo) => {
-// 		return todo.date;
-// 	})
-// );
-
-// console.log(
-// 	todoItems.map(fucntion(todo) {
-// 		todo.date
-// 	})
-// );
+addItem("todo item #2");
+addItem("todo item #3");
+addItem("todo item #4");
--- test/test.js
+++ test/test.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+// console.log("hello world");
+let items = [];
+const posts = () => {
+	const postsList = {
+		id: Date.now(),
+		title: "Sample Title 1",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+	};
+	items.push(postsList);
+};
+// const postIds = posts.map((asdfasd) => asdfasd.title);
+
+const postIdsObject = items.((asdfasd) => asdfasd.id);
