54f7e4694aa029552729eb413c998b4691a3945b
Carlos Tabangay <RedDotz20@protonmail.com>
2022-08-04 18:15:52 +0800

    working todo-list

--- src/js/function.js
+++ src/js/function.js
@@ -1,21 +1,43 @@
 // import { currentDate, currentTime } from "../components/dateAndTime.js";
-const todoInputItem = document.getElementById("todoInput");
-const todoAddButton = document.getElementById("todoaddbutton");
-const todoList = document.getElementById("todos");
 
-const newList = document.createElement("li");
-const newButton = document.createElement("button");
+const addTask = document.querySelector("#add-task");
+const inputTask = document.querySelector("#input-task");
+const taskContainer = document.querySelector("#tasks-container");
 
-function addTodoItems(button, list, itemAdd) {
-	button.addEventListener("click", function () {
-		let newNodeElement = document.createElement("li");
-		let newTextNode = document.createTextNode(itemAdd.value);
-		newNodeElement.appendChild(newTextNode);
-		list.appendChild(newNodeElement);
+addTask.addEventListener("click", function () {
+	// creating a div element
+	let task = document.createElement("div");
+	task.classList.add("task");
+
+	// creating list item with value
+	let li = document.createElement("li");
+	li.innerText = `${inputTask.value}`;
+	task.appendChild(li);
+
+	let checkButton = document.createElement("button");
+	checkButton.innerHTML = "check";
+	checkButton.classList.add("checkTask");
+	task.appendChild(checkButton);
+
+	let deleteButton = document.createElement("button");
+	deleteButton.innerHTML = "delete";
+	deleteButton.classList.add("deleteTask");
+	task.appendChild(deleteButton);
+
+	inputTask.value === ""
+		? alert("please enter a task")
+		: taskContainer.appendChild(task);
+
+	inputTask.value = "";
+
+	// CHECKED BUTTON EVENT
+	checkButton.addEventListener("click", function () {
+		checkButton.parentElement.style.textDecoration = "line-through";
 	});
-}
 
-function removeTodoItems(button, list, itemRemove) {}
-
-// Add New Items
-addTodoItems(todoAddButton, todoList, todoInputItem);
+	// DELETE BUTTON EVENT
+	deleteButton.addEventListener("click", function (e) {
+		let target = e.target;
+		target.parentElement.parentElement.remove();
+	});
+});
