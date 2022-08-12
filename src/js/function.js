acaadf7a9ac3c8d25067f518643f17b3581e4ec4
Carlos Tabangay <RedDotz20@protonmail.com>
2022-08-12 20:07:08 +0800

    function.js refactored

--- src/js/function.js
+++ src/js/function.js
@@ -4,12 +4,16 @@ const addTask = document.getElementById("add-task");
 const inputTask = document.getElementById("input-task");
 const taskContainer = document.getElementById("tasks-container");
 
+//! Enter Key Event
 inputTask.addEventListener("keyup", (e) => {
 	e.preventDefault();
 	if (e.keyCode === 13) addTask.click();
 });
 
+//! Button Event
 addTask.addEventListener("click", (e) => {
+	//* Creating "task" Container
+
 	let task = document.createElement("div");
 	task.classList.add("task");
 
@@ -17,55 +21,53 @@ addTask.addEventListener("click", (e) => {
 	li.innerText = `${inputTask.value}`;
 	task.appendChild(li);
 
-	// ----------------------------------------
+	//* Appending Check Button to Task
 
 	let checkButton = document.createElement("button");
+	checkButton.classList.add("check-btn", "task-button");
+	checkButton.innerHTML = "CHECKED";
+	task.appendChild(checkButton);
 
+	// TODO: SVG ICON append to checkButton
 	// let chkImg = document.createElement("img");
 	// chkImg.setAttribute("src", "/img/check-solid.svg");
 	// checkButton.appendChild(chkImg);
-	checkButton.innerHTML = "CHECKED";
 
-	checkButton.classList.add("check-btn");
-	checkButton.classList.add("task-button");
-	task.appendChild(checkButton);
-
-	// ----------------------------------------
+	//* Appending Delete Button to Task
 
 	let deleteButton = document.createElement("button");
+	deleteButton.classList.add("delete-btn", "task-button");
+	deleteButton.innerHTML = "DELETE";
+	task.appendChild(deleteButton);
 
+	// TODO: SVG ICON append to deleteButton
 	// let delImg = document.createElement("img");
 	// delImg.setAttribute("src", "/img/trash-solid.svg");
 	// deleteButton.appendChild(delImg);
-	deleteButton.innerHTML = "DELETE";
 
-	deleteButton.classList.add("delete-btn");
-	deleteButton.classList.add("task-button");
-	task.appendChild(deleteButton);
+	//* Appending Date and Time Variables to Task
 
-	// ----------------------------------------
-
-	let time = currentTime;
-	let date = currentDate;
 	let dateContainer = document.createElement("div");
-	dateContainer.classList.add("dateAndTime");
-
 	let dateTime = document.createElement("span");
-	dateTime.textContent = `${time} - ${date}`;
+	dateContainer.classList.add("dateAndTime");
+	dateTime.textContent = `${currentTime} - ${currentDate}`;
 	dateContainer.appendChild(dateTime);
 	task.appendChild(dateContainer);
+
+	//* Task Input Validation (empty string)
 
 	inputTask.value === ""
 		? alert("Please Enter a Task")
 		: taskContainer.appendChild(task);
+	inputTask.value = ""; // delete input value
 
-	inputTask.value = "";
-
+	//! Check Button Event
 	checkButton.addEventListener("click", () => {
 		checkButton.previousElementSibling.style.textDecoration =
 			"line-through";
 	});
 
+	//! Remove Button Event
 	deleteButton.addEventListener("click", (e) => {
 		let target = e.target;
 		target.parentElement.remove();
