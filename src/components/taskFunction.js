635ce45a5e6761b9d86c679b300f20c266635657
Carlos Tabangay <carlos_tabangay@outlook.com>
2022-09-05 02:42:04 +0800

    Task Done Event modified
    UI adjustments for task container

--- src/components/taskFunction.js
+++ src/components/taskFunction.js
@@ -16,11 +16,26 @@ export function createTaskFunction() {
 	//* Creating Task Container | Getting Input Value
 	let newTask = document.createElement("div");
 	newTask.classList.add("task");
-	let taskValContainer = document.createElement("ul");
-	let taskValue = document.createElement("li");
-	taskValue.innerText = `${inputTask.value}`;
+
+	let taskValContainer = document.createElement("div");
+	taskValContainer.classList.add("content");
+
+	let taskValue = document.createElement("input");
+	// taskValue.innerText = `${inputTask.value}`;
+
+	taskValue.classList.add("text");
+	taskValue.type = "text";
+	taskValue.value = inputTask.value;
+	taskValue.setAttribute("readonly", "readonly");
+
 	taskValContainer.appendChild(taskValue);
 	newTask.appendChild(taskValContainer);
+
+	//* Creating and Appending Edit Button to Task
+	let editTaskButton = document.createElement("button");
+	editTaskButton.classList.add("edit");
+	editTaskButton.innerText = "EDIT";
+	newTask.appendChild(editTaskButton);
 
 	//* Creating and Appending Check Button to Task
 	let checkButton = document.createElement("i");
@@ -34,22 +49,39 @@ export function createTaskFunction() {
 	deleteButton.innerText = "delete";
 	newTask.appendChild(deleteButton);
 
-	//* Appending Date and Time Variables to Task
-	// let dateContainer = document.createElement("div");
-	// let dateTime = document.createElement("span");
-	// dateContainer.classList.add("dateAndTime");
-	// dateTime.textContent = `${currentTime} - ${currentDate}`;
-	// dateContainer.appendChild(dateTime);
-	// newTaskBtn.appendChild(dateContainer);
-
-	//! Check Button Event
-	checkButton.addEventListener("click", (e) => {
-		let checkTarget = e.target.previousElementSibling.firstChild;
-		checkTarget.style.textDecoration = "line-through";
-		checkTarget.style.color = "rgb(0, 255, 0)";
+	//! Edit Button Event Handler
+	editTaskButton.addEventListener("click", (e) => {
+		let checkDone = e.target.nextElementSibling;
+		if (editTaskButton.innerText == "EDIT") {
+			if (!checkDone.classList.contains("taskChecked")) {
+				editTaskButton.innerText = "SAVE";
+				taskValue.removeAttribute("readonly");
+				taskValue.focus();
+			}
+		} else {
+			editTaskButton.innerText = "EDIT";
+			taskValue.setAttribute("readonly", "readonly");
+		}
 	});
 
-	//! Remove Button Event
+	//! Check Button Event Handler
+	checkButton.addEventListener("click", (e) => {
+		let checkTarget =
+			e.target.previousElementSibling.previousElementSibling.firstChild;
+		if (checkButton.classList.contains("taskChecked")) {
+			checkButton.classList.remove("taskChecked");
+			checkTarget.style.color = null;
+			checkTarget.style.textDecoration = null;
+			checkTarget.style.transition = "200ms";
+		} else {
+			checkButton.classList.add("taskChecked");
+			checkTarget.style.color = "rgb(0, 255, 0)";
+			checkTarget.style.textDecoration = "line-through";
+			checkTarget.style.transition = "350ms";
+		}
+	});
+
+	//! Remove Button Event Handler
 	deleteButton.addEventListener("click", (e) => {
 		let target = e.target;
 		target.parentElement.remove();
@@ -67,3 +99,11 @@ export function createTaskFunction() {
 		return taskContainer.prepend(newTask);
 	}
 }
+
+//* Appending Date and Time Variables to Task
+// let dateContainer = document.createElement("div");
+// let dateTime = document.createElement("span");
+// dateContainer.classList.add("dateAndTime");
+// dateTime.textContent = `${currentTime} - ${currentDate}`;
+// dateContainer.appendChild(dateTime);
+// newTaskBtn.appendChild(dateContainer);
