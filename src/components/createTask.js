375d9554c5217bc75aad0cf2a0814afd417232e0
RedDotz20 <carlos_tabangay@outlook.com>
2023-06-24 23:18:06 +0800

    refactor: dir

--- src/components/createTask.js
+++ src/components/createTask.js
@@ -1,67 +1,89 @@
-import { editBtnFunction, validateCheckBox } from "./taskEvent.js";
+import { editBtnFunction, validateCheckBox } from './taskEvent.js';
 
-const taskContainer = document.getElementById("tasks-container");
-export const inputTask = document.getElementById("input-task");
+const taskContainer = document.getElementById('tasks-container');
+export const inputTask = document.getElementById('input-task');
 
 export function createTaskFunction() {
-	//* Creating Task Container | Getting User Input Value
-	let newTask = document.createElement("div");
-	newTask.classList.add("task");
+	const taskValue = inputTask.value.trim();
 
-	let taskValContainer = document.createElement("div");
-	taskValContainer.classList.add("content");
+	if (taskValue === '') {
+		displayAlert('Please enter a task.');
+		return;
+	}
 
-	//* New Check button
-	const checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
-	// checkbox.name = "name";
-	// checkbox.value = "value";
-	checkbox.id = "checkboxId";
+	const newTask = document.createElement('div');
+	newTask.classList.add('task');
+
+	const checkbox = createCheckbox();
+	checkbox.addEventListener('click', validateCheckBox);
 	newTask.appendChild(checkbox);
 
-	checkbox.addEventListener("click", validateCheckBox);
-
-	let taskValue = document.createElement("input");
-	taskValue.classList.add("text");
-	taskValue.type = "text";
-	taskValue.value = inputTask.value;
-	taskValue.setAttribute("readonly", "readonly");
-
-	taskValContainer.appendChild(taskValue);
+	const taskValContainer = createTaskValueContainer(taskValue);
 	newTask.appendChild(taskValContainer);
 
-	//* Creating and Appending Edit Button to Task
-	let editTaskButton = document.createElement("button");
-	editTaskButton.classList.add("edit");
-	editTaskButton.innerText = "EDIT";
+	const editTaskButton = createButton('EDIT', 'edit', editBtnFunction);
 	newTask.appendChild(editTaskButton);
 
-	//! Edit Button Event Handler
-	editTaskButton.addEventListener("click", editBtnFunction);
-
-	//* Creating and Appending Delete Button to Task
-	let deleteButton = document.createElement("i");
-	deleteButton.classList.add("material-icons", "delete-btn");
-	deleteButton.innerText = "delete";
+	const deleteButton = createDeleteButton();
+	deleteButton.addEventListener('click', deleteTask);
 	newTask.appendChild(deleteButton);
 
-	//! Delete Button Event Handler
-	deleteButton.addEventListener("click", (e) => {
-		let target = e.target;
-		target.parentElement.remove();
+	inputTask.value = '';
+	taskContainer.prepend(newTask);
+	console.log('New task added');
+}
+
+function displayAlert(message) {
+	const alertBoxContainer = document.getElementById('alertBox-container');
+	alertBoxContainer.style.display = 'block';
+
+	const closeAlert = document.getElementById('alertClose-btn');
+	closeAlert.addEventListener('click', () => {
+		alertBoxContainer.style.display = 'none';
 	});
 
-	//* Task Input Validation (empty field)
-	let openAlert = document.getElementById("alertBox-container");
-	let closeAlert = document.getElementById("alertClose-btn");
-	if (inputTask.value === "") {
-		openAlert.style.display = "block";
-		closeAlert.addEventListener("click", () => {
-			openAlert.style.display = "none";
-		});
-	} else {
-		inputTask.value = "";
-		console.log("new task added");
-		return taskContainer.prepend(newTask);
-	}
+	const alertMessage = document.getElementById('alertMessage');
+	alertMessage.textContent = message;
+}
+
+function createCheckbox() {
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+	checkbox.id = 'checkboxId';
+	return checkbox;
+}
+
+function createTaskValueContainer(taskValue) {
+	const taskValContainer = document.createElement('div');
+	taskValContainer.classList.add('content');
+
+	const taskValueInput = document.createElement('input');
+	taskValueInput.classList.add('text');
+	taskValueInput.type = 'text';
+	taskValueInput.value = taskValue;
+	taskValueInput.setAttribute('readonly', 'readonly');
+
+	taskValContainer.appendChild(taskValueInput);
+
+	return taskValContainer;
+}
+
+function createButton(text, className, clickHandler) {
+	const button = document.createElement('button');
+	button.classList.add(className);
+	button.innerText = text;
+	button.addEventListener('click', clickHandler);
+	return button;
+}
+
+function createDeleteButton() {
+	const deleteButton = document.createElement('i');
+	deleteButton.classList.add('material-icons', 'delete-btn');
+	deleteButton.innerText = 'delete';
+	return deleteButton;
+}
+
+function deleteTask(e) {
+	const target = e.target;
+	target.parentElement.remove();
 }
--- src/components/taskEvent.js
+++ src/components/taskEvent.js
@@ -1,23 +1,25 @@
 export const editBtnFunction = (e) => {
-	let checkDone = e.target.nextElementSibling.classList;
-	let textInputTarget = e.target.previousElementSibling.firstChild;
+	const checkDone = e.target.nextElementSibling.classList;
+	const textInputTarget = e.target.previousElementSibling.firstChild;
 
-	if (e.target.innerText == "EDIT") {
-		if (!checkDone.contains("taskChecked")) {
-			e.target.innerText = "SAVE";
-			textInputTarget.removeAttribute("readonly");
+	if (e.target.innerText === 'EDIT') {
+		if (!checkDone.contains('taskChecked')) {
+			e.target.innerText = 'SAVE';
+			textInputTarget.removeAttribute('readonly');
 			textInputTarget.focus();
 		}
 	} else {
-		e.target.innerText = "EDIT";
-		textInputTarget.setAttribute("readonly", "readonly");
+		e.target.innerText = 'EDIT';
+		textInputTarget.setAttribute('readonly', 'readonly');
 	}
 };
 
 export const validateCheckBox = (e) => {
-	let target = e.target;
-	let task = e.target.nextElementSibling.firstChild;
-	!target.checked
-		? task.classList.remove("taskChecked")
-		: task.classList.add("taskChecked");
+	const target = e.target;
+	const task = e.target.nextElementSibling.firstChild;
+	if (!target.checked) {
+		task.classList.remove('taskChecked');
+	} else {
+		task.classList.add('taskChecked');
+	}
 };
